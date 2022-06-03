1b0d92822e0e7af74f22457b2e4b47cbb28b103d
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2022-06-03 06:47:32 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 ln -sb $(pwd)/vimrc ~/.vimrc
-unlink ~/.vim/after/ftplugin
+if [ ! -d ~/.vim/after ]; then
+    mkdir ~/.vim/after
+fi
+if [ -e ~/.vim/after/ftplugin ]; then
+    echo "error: after/ftplugin exists"
+    exit
+fi
 ln -s $(pwd)/ftplugin ~/.vim/after
