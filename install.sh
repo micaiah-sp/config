31bb0d3cc68a2e96bb122d4d7e345a0cab209550
micaiah <micaiah@smithpierce.net>
2024-05-28 12:38:12 -0400

    better virtual environment setup

--- install.sh
+++ install.sh
@@ -9,21 +9,13 @@ fi
 if [ ! -d ~/.vim/spelling ]; then
     mkdir ~/.vim/spelling
 fi
-if [ -e ~/.vim/after/ftplugin ]; then
-    echo "error: after/ftplugin exists"
-    exit
-fi
-if [ -e ~/.vim/after/ftdetect ]; then
-    echo "error: after/ftdetect exists"
-    exit
-fi
-ln -s $(pwd)/ftplugin ~/.vim/after
-ln -s $(pwd)/ftdetect ~/.vim/after
-ln -s $(pwd)/mybashrc ~/.mybashrc
+ln -sf $(pwd)/ftplugin ~/.vim/after
+ln -sf $(pwd)/ftdetect ~/.vim/after
+ln -sf $(pwd)/mybashrc ~/.mybashrc
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 
-ln -s $(pwd)/usercustomize.py $(python3 -m site --user-site)
-python3 -m venv ~/main_py_env
+python3 -m venv ~/.main_venv
+ln -sf $(pwd)/usercustomize.py ~/.main_venv/lib/python3*/site-packages/
