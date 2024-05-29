5ce194e0c82f308cc2d7130f75754063b33f3ae0
micaiah <micaiah@smithpierce.net>
2024-05-29 16:08:08 -0400

    install python packages

--- install.sh
+++ install.sh
@@ -18,4 +18,6 @@ fi
 vim +PluginInstall +qall
 
 python3 -m venv ~/.main_venv
-ln -sf $(pwd)/usercustomize.py ~/.main_venv/lib/python3*/site-packages/
+#ln -sf $(pwd)/usercustomize.py ~/.main_venv/lib/python3*/site-packages/
+source ~/.main_venv/bin/activate
+pip3 install numpy scipy matplotlib pandas sympy termcolor gitpython
