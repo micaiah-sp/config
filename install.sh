aec29faa9659b10623b41e3740a7d59630ae86fb
micaiah <micaiah@smithpierce.net>
2026-01-04 18:16:07 -0500

    new vim-related scripts

--- install.sh
+++ install.sh
@@ -40,5 +40,7 @@ vim +PluginInstall +qall
 python3 -m venv ~/.main_venv
 #ln -sf $(pwd)/usercustomize.py ~/.main_venv/lib/python3*/site-packages/
 ln -sf $(pwd)/cpp-open ~/.main_venv/bin/
+ln -sf $(pwd)/cpp-create ~/.main_venv/bin/
+ln -sf $(pwd)/cpp-cropen ~/.main_venv/bin/
 source ~/.main_venv/bin/activate
 pip3 install numpy scipy matplotlib pandas sympy termcolor gitpython
